5d9d50b7bc2c310f54fa1088c508b727b21d0486
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-26 13:53:11 -0400

    restricted middleware complete

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { jstSecret } = require('../../config/secret');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({
+      message: 'Token Required',
+    });
+  } else {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({
+          message: 'Token Invalid',
+        });
+      } else {
+        req.decodeJwt = decoded;
+        next();
+      }
+    });
+  }
 };
